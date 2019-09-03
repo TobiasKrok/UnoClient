b751dbb4f0ccacbb03b3f73fbee1ee33954d8467
Tobias <35228879+Tennoo@users.noreply.github.com>
2019-09-03 23:08:28 +0200

    update

--- src/main/java/com/tobias/Main.java
+++ src/main/java/com/tobias/Main.java
@@ -2,20 +2,24 @@ package com.tobias;
 
 
 import com.tobias.server.ServerConnection;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.io.IOException;
 import java.net.Socket;
 
 public class Main {
-
+private static Logger LOGGER = LogManager.getLogger(Main.class.getName());
     public static void main(String[] args) {
         try {
             Socket socket = new Socket("localhost", 5000);
-            ServerConnection serverConnection = new ServerConnection(socket);
-            new Thread(serverConnection).start();
+            if(socket.isConnected()) {
+                ServerConnection serverConnection = new ServerConnection(socket);
+                new Thread(serverConnection).start();
+            }
 
         } catch (IOException e) {
-            System.out.println("Socket error: " + e.getMessage());
+            LOGGER.fatal("Failed to connect to server!");
         }
     }
 }
