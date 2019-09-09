afc675482c592c607417f2084f9bae13dfdbf3b9
Tobias <35228879+Tennoo@users.noreply.github.com>
2019-09-09 23:22:00 +0200

    update

--- src/main/java/com/tobias/server/handlers/GameCommandHandler.java
+++ src/main/java/com/tobias/server/handlers/GameCommandHandler.java
@@ -1,9 +1,17 @@
 package com.tobias.server.handlers;
 
 import com.tobias.game.GameManager;
+import com.tobias.game.card.Card;
+import com.tobias.game.card.CardColor;
+import com.tobias.game.card.CardType;
 import com.tobias.server.ServerConnection;
 import com.tobias.server.command.Command;
 import com.tobias.server.command.CommandType;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 
 public class GameCommandHandler implements CommandHandler {
@@ -17,8 +25,20 @@ public class GameCommandHandler implements CommandHandler {
     @Override
     public void process(Command command) {
         if(command.getType() == CommandType.PLAYER_DRAWCARD) {
-            System.out.println("card received " + command.getData());
+            
         }
     }
 
+    private List<Card> parseCards(String cardStr) {
+        List<Card> cards = new ArrayList<>();
+        Pattern p = Pattern.compile("\\[(.*?)\\]");
+        Matcher m = p.matcher(cardStr);
+        String[] props;
+        while(m.find()) {
+            props = m.group(1).split(",");
+            Card card = new Card(CardType.valueOf(props[0]), CardColor.valueOf(props[1]),Integer.parseInt(props[2]));
+            cards.add(card);
+        }
+        return cards;
+    }
 }
